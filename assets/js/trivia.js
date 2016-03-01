d734ccf1f8faa1d9ecd10b849088a33666a95d1d
@angryjenkins <matt@angryjenkins.com>
2016-03-01 00:57:28 -0500

    working on trivia

---
+++ assets/js/trivia.js
@@ -0,0 +1,25 @@
+
+function run(){
+	var time = 30;
+
+  	counter = setInterval(increment, 1000);
+}
+
+function trivia(){
+	var question = ["Who was the first African American female to host a television show?",
+					"Who was the first African American billionaire?",
+					"Who was the first African American Secretary of State?",
+					"Who said 'Nothing in all the world is more dangerous than sincere ignorance and conscientious stupidity.'?",
+					"In what year was slavery abolished in America?"]
+
+	var answer = 	[
+						["Oprah Winfrey","Whoopi Goldberg","Ella Fitzgerald","Hattie McDaniel"],
+						["Robert Johnson","Frederick Douglass","Oprah Winfrey","W.E.B. DuBois"],
+						["Martin Luther King Jr.","Malcolm X","Frederick Douglass","Jesse Jackson"],
+						["Colin Powell","Condoleeza Rice","Jesse L. Brown","Frederick Douglass"],
+						["1865","1863","1881","1920"]
+					]
+
+
+
+}
