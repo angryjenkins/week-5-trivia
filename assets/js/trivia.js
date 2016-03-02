31047cb8757dd2c8efae82b55f9a89a975dd5225
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-01 19:37:11 -0500

    OK let's bang out this homework, ...

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,27 +1,8 @@
+$(document).ready(function(){
+	//initialize all questions.
 
-function run(){
-	var time = 30;
-
-  	counter = setInterval(increment, 1000);
-}
-
-function trivia(){
-	var question = ["Who was the first African American female to host a television show?",
-					"Who was the first African American billionaire?",
-					"Who was the first African American Secretary of State?",
-					"Who said 'Nothing in all the world is more dangerous than sincere ignorance and conscientious stupidity.'?",
-					"In what year was slavery abolished in America?",
-					"Who wrote 'What happens to a dream deferred? / Does it dry up / like a raisin in the sun?'"]
-
-	var answer = 	[
-						["Oprah Winfrey","Whoopi Goldberg","Ella Fitzgerald","Hattie McDaniel"],
-						["Robert Johnson","Frederick Douglass","Oprah Winfrey","W.E.B. DuBois"],
-						["Martin Luther King Jr.","Malcolm X","Frederick Douglass","Jesse Jackson"],
-						["Colin Powell","Condoleeza Rice","Jesse L. Brown","Frederick Douglass"],
-						["1865","1863","1881","1920"]
-						["Langston Hughes","Richard Wright","Toni Morrison","Phyllis Wheatley"]
-					]
+	$(".round").hide();
 
 
 
-}
+});
